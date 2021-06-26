65bf0a4ab5ed0660b57f48796940d8d5ff4dc4f4
nivinjoseph <nivinjoseph@outlook.com>
2021-06-25 21:21:06 -0400

    added tests

--- test/eda.test.ts
+++ test/eda.test.ts
@@ -1,5 +1,6 @@
 import { Delay } from "@nivinjoseph/n-util";
 import * as Assert from "assert";
+import { EdaEvent } from "../src";
 import { EdaManager } from "../src/eda-manager";
 import { EventBus } from "../src/event-bus";
 import { createEdaManager, EventHistory, TestEvent } from "./utils/eda-test-utils";
@@ -19,22 +20,63 @@ suite.only("eda tests", () =>
         await edaManager.dispose();
     });
     
+    // test("basic", async () =>
+    // {
+    //     // @ts-ignore
+    //     const consumePromise = edaManager.beginConsumption();
+    //     const eventBus = edaManager.serviceLocator.resolve<EventBus>("EventBus");
+    //     const history = edaManager.serviceLocator.resolve<EventHistory>("EventHistory");
+    //     const testEvent = new TestEvent({ id: "1-foo" });
+    //     const eventIds = [testEvent.id];
+        
+    //     await eventBus.publish("basic", testEvent);
+        
+    //     await Delay.seconds(2);
+        
+    //     // Assert.ok(history.records.length === 1 && history.records[0] === testEvent.id);
+        
+    //     Assert.strictEqual(history.records.length, 1, "number of records don't match");
+    //     Assert.deepStrictEqual(history.records, eventIds, "eventIds don't match");
+    // });
+    
+    
     test("basic", async () =>
     {
         // @ts-ignore
         const consumePromise = edaManager.beginConsumption();
         const eventBus = edaManager.serviceLocator.resolve<EventBus>("EventBus");
         const history = edaManager.serviceLocator.resolve<EventHistory>("EventHistory");
-        const testEvent = new TestEvent({ id: "1-foo" });
-        const eventIds = [testEvent.id];
+        const testEvents = new Array<EdaEvent>();
         
-        await eventBus.publish("basic", testEvent);
+        for (let i = 0; i < 100; i++)
+        {
+            for (let j = 0; j < 100; j++)
+            {
+                const event = new TestEvent({ id: `test_${i}-evt_${j}` });
+                testEvents.push(event);
+            }
+        }
         
-        await Delay.seconds(2);
-        
+        const numbers = testEvents.map(t => Number.parseInt(t.id.split("-")[1].split("_")[1]));
+            
+
+        await eventBus.publish("basic", ...testEvents);
+
+        await Delay.minutes(3);
+
         // Assert.ok(history.records.length === 1 && history.records[0] === testEvent.id);
+
+        const historyIds = history.records.groupBy(t => t.split("-")[0]).reduce((acc, t) =>
+        {
+            acc.push(...t.values);
+            return acc;
+        }, new Array<string>());
         
-        Assert.strictEqual(history.records.length, 1, "number of records don't match");
-        Assert.deepStrictEqual(history.records, eventIds, "eventIds don't match");
+        const historyNumbers = historyIds.map(t => Number.parseInt(t.split("-")[1].split("_")[1]));
+        
+        Assert.strictEqual(history.records.length, testEvents.length, "number of records don't match");
+        // console.log(historyIds);
+        // Assert.deepStrictEqual(historyIds, testEvents.map(t => t.id), "eventIds don't match");
+        Assert.deepStrictEqual(historyNumbers, numbers, "numbers don't match");
     });
 });
