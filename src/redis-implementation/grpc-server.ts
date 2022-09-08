6e7dcdad109c00ca5a997b9d15b87bf3f01c9f75
Nivin Joseph <nivinjoseph@outlook.com>
2022-09-08 09:56:04 -0400

    fixed proto path issue

--- src/redis-implementation/grpc-server.ts
+++ src/redis-implementation/grpc-server.ts
@@ -218,8 +218,8 @@ export class GrpcServer
             oneofs: true
         };
 
-        const basePath = __dirname.endsWith("dist")
-            ? Path.resolve(__dirname, "..", "src", "redis-implementation")
+        const basePath = __dirname.endsWith(`dist${Path.sep}redis-implementation`)
+            ? Path.resolve(__dirname, "..", "..", "src", "redis-implementation")
             : __dirname;
         
         const packageDef = ProtoLoader.loadSync(Path.join(basePath, "grpc-processor.proto"), options);
